547915228fcd86e261b426cef8d30629dfadb1d8
mkbakhtiar <mkbakhtiar.new@gmail.com>
2024-09-16 01:03:14 +0700

    add count & change props option to option.name becouse adjsut from be field

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -9,65 +9,68 @@
         <h1 class="text-3xl w-auto px-4 absolute bottom-1">I'm looking for...</h1>
       </div>
 
-      <!-- Filter dropdowns -->
-      <div class="grid grid-cols-5 gap-4 py-4 px-4 rounded-r-lg bg-white -mt-4">
-        <div v-for="filter in filters" :key="filter.name" class="relative">
-          <div 
-            @click="toggleFilterList(filter)"
-            class="w-full p-2 pr-8 bg-gray-200 text-gray-700 border border-gray-300 rounded-lg cursor-pointer flex flex-col items-start justify-between relative"
-          >
-            <!-- Display filter name on top -->
-            <span class="text-sm text-gray-500">{{ filter.name }}</span>
+      <div class="flex justify-between bg-white p-4 -mt-4">
+        <!-- Filter dropdowns -->
+        <div class="w-full mr-4 grid grid-cols-4 gap-4 rounded-r-lg bg-white">
+          <div v-for="filter in filters" :key="filter.name" class="relative">
+            <div 
+              @click="toggleFilterList(filter)"
+              class="w-full p-3 pr-5 bg-gray-100 text-gray-700 border border-gray-300 rounded-lg cursor-pointer flex flex-col items-start justify-between relative"
+            >
+              <!-- Display filter name on top -->
+              <span class="text-xs text-gray-500 mb-2">{{ filter.name }}</span>
 
-            <!-- Display selected value below the filter name -->
-            <div class="w-full flex justify-between items-center">
-              <span>{{ filter.selected || 'Select ' + filter.name }}</span>
-
-              <!-- Clear button -->
-              <button 
-                v-if="filter.selected" 
-                @click.stop="clearFilter(filter)" 
-                class="ml-2 w-6 h-6 bg-gray-200 text-gray-700 rounded-full flex items-center justify-center">
-                <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                </svg>
-              </button>
-              
-              <!-- Dropdown arrow -->
-              <svg class="w-4 h-4 text-orange-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
-            </div>
-          </div>
-          
-          <!-- Custom dropdown for each filter -->
-          <div v-if="filter.showList" class="bg-white border border-gray-300 rounded-lg shadow-sm py-4 absolute z-10 mt-2 w-full">
-            <div class="px-4">
-              <h3 class="text-gray-700 mb-2">Select a {{ filter.name }}</h3>
-              <div class="relative">
-                <input 
-                  v-model="filter.search" 
-                  type="text" 
-                  :placeholder="`Search ${filter.name}`" 
-                  class="w-full p-2 border border-gray-300 rounded-lg mb-2"
-                >
-                <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                  <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+              <!-- Display selected value below the filter name -->
+              <div class="w-full flex justify-between items-center">
+                <span class="text-orange-500 text-sm font-bold">{{ filter.selected || 'ALL'}}</span>
+                <div class="flex items-center gap-x-1">
+                  <!-- Clear button -->
+                  <button 
+                    v-if="filter.selected" 
+                    @click.stop="clearFilter(filter)" 
+                    class="ml-2 w-6 h-6 bg-gray-200 text-gray-700 rounded-full flex items-center justify-center">
+                    <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                  </button>
+                  
+                  <!-- Dropdown arrow -->
+                  <svg class="w-4 h-4 text-orange-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
                 </div>
               </div>
             </div>
-            <ul class="space-y-2 max-h-60 overflow-y-auto">
-              <li 
-                v-for="option in getFilteredOptions(filter)" 
-                :key="option" 
-                @click="selectFilterOption(filter, option)"
-                class="flex justify-between items-center cursor-pointer hover:bg-gray-100 p-1 px-4 rounded"
-              >
-                <span class="text-gray-700">{{ option }}</span>
-                <span v-if="option.count" class="text-orange-500">{{ option.count }}</span>
-              </li>
-            </ul>
+            
+            <!-- Custom dropdown for each filter -->
+            <div v-if="filter.showList" class="bg-white border border-gray-300 rounded-lg shadow-sm py-4 absolute z-10 mt-2 w-full">
+              <div class="px-4">
+                <h3 class="text-gray-700 mb-2">Select a {{ filter.name }}</h3>
+                <div class="relative">
+                  <input 
+                    v-model="filter.search" 
+                    type="text" 
+                    :placeholder="`Search ${filter.name}`" 
+                    class="w-full p-2 border border-gray-300 rounded-lg mb-2"
+                  >
+                  <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                    <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                  </div>
+                </div>
+              </div>
+              <ul class="space-y-2 max-h-60 overflow-y-auto">
+                <li 
+                  v-for="option in getFilteredOptions(filter)" 
+                  :key="option" 
+                  @click="selectFilterOption(filter, option)"
+                  class="flex justify-between items-center cursor-pointer hover:bg-gray-100 p-1 px-4 rounded"
+                >
+                  <span class="text-gray-700">{{ option.name }}</span>
+                  <span v-if="option.count" class="text-orange-500">{{ option.count }}</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <button class="bg-orange-500 text-white font-bold py-2 px-4 rounded">
+        <button class="bg-orange-500 text-white font-bold py-2 px-4 rounded-lg flex flex-col block justify-center items-center">
           <font-awesome-icon icon="search" size="xl" />
           Find
         </button>
@@ -149,14 +152,17 @@ export default {
       }
       
       return options.filter(option => 
-        option.toLowerCase().includes(filter.search.toLowerCase())
+        option.name.toLowerCase().includes(filter.search.toLowerCase())
       );
     },
     clearFilter(filter) {
       filter.selected = ''; // Clear the selected value
+      filter.showList = false;
+      // Reload filter data when a new option is selected
+      this.loadFilterData();
     },
     selectFilterOption(filter, option) {
-      filter.selected = option;
+      filter.selected = option.name;
       filter.showList = false;
       // Reload filter data when a new option is selected
       this.loadFilterData();
